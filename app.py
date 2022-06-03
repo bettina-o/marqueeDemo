345b96a6ca51b61821a6d54aeb1f8e7d2df0c505
Bettina Oghinan <106831717+bettina-o@users.noreply.github.com>
2022-06-03 14:44:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ st.header("Header")
 
 categories = ['a','b','c']
 st.multiselect("Pick a category", categories)
-if st.button("Click me"):
-  st.write("Why did you not select instead?")
+st.sidebar.button("Click me")
+
+#by putting the word sidebar, it does it in a menu which can be hidden
 if st.checkbox("Select me!"):
   st.write("You selected the checkbox!")
   
